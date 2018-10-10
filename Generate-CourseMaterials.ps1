9c7defb720c827242db6d0b2de04c2e48337abb6
Kamran Ayub <kamran.ayub@gmail.com>
2018-10-09 20:46:25 -0500

    Just fold common files into all module zips

--- Generate-CourseMaterials.ps1
+++ Generate-CourseMaterials.ps1
@@ -2,19 +2,18 @@ if (!(Test-Path .\.materials)) {
     mkdir .\.materials
 }
 
-# All Modules
-Compress-Archive -DestinationPath .\.materials\All-Modules.zip -Update -Path @(
+$CommonPaths = @(
     ".\example-site-files",
     ".\LICENSE",
     ".\README.md"
 )
 
 # Module 3
-Compress-Archive -DestinationPath .\.materials\Module-3.zip -Update -Path @(
+Compress-Archive -DestinationPath .\.materials\Module-3.zip -Update -Path ($CommonPaths + @(
     ".\Module 3"
-)
+))
 
 # Module 4
-Compress-Archive -DestinationPath .\.materials\Module-4.zip -Update -Path @(
+Compress-Archive -DestinationPath .\.materials\Module-4.zip -Update -Path ($CommonPaths + @(
     ".\Module 4"
-)
+))
