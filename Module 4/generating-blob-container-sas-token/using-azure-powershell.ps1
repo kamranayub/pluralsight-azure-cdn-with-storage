bd8ec7b25db70468f74fd059c4fa34f7b0491ea5
Kamran Ayub <kamran.ayub@gmail.com>
2019-06-23 16:36:45 -0500

    Fix cmdlet

--- Module 4/generating-blob-container-sas-token/using-azure-powershell.ps1
+++ Module 4/generating-blob-container-sas-token/using-azure-powershell.ps1
@@ -7,7 +7,7 @@ $StorageAccount = Get-AzStorageAccount `
 # Stored Access Policy created in the last demo.
 # We pass in our container name, access policy name,
 # and the storage context to use
-New-AzureStorageContainerSASToken `
+New-AzStorageContainerSASToken `
     -Container staticsite `
     -Policy cdn_v1 `
     -Context $StorageAccount.Context 
